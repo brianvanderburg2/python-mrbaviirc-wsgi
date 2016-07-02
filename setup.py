6bb031e96e831584323314c9f29c84592486173f
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2016-07-01 21:12:03 -0400

    Minor changes to layout.  Now with setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="mrbavii",
+    version="0.1",
+    description="Mr. Brian Allen Vanderburg II Reusable Code",
+    author = "Brian Allen Vanderburg II",
+    packages=["mrbavii"],
+    namespace_packages=["mrbavii"]
+
+)
