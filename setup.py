96110e9724300912362ed16f78eddbbc45532365
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2016-07-02 22:45:19 -0400

    Use find_packages

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="mrbavii",
     version="0.1",
     description="Mr. Brian Allen Vanderburg II Reusable Code",
     author = "Brian Allen Vanderburg II",
-    packages=["mrbavii"],
+    packages=find_packages(),
     namespace_packages=["mrbavii"]
-
 )
